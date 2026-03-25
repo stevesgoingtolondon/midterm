77437dcac45481b01aa08717ced922b5435d5520
Kat Morris <kemorris2@my.waketech.edu>
2026-03-25 23:56:41 +0000

    hi

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 var p1 = document.getElementById("p1");
 var p2 = document.getElementById("p2");
 var p3 = document.getElementById("p3");
-
 var img = document.getElementById("story-image");
 var choicesDiv = document.getElementById("choices");
-
 var nextBtn = document.getElementById("nextBtn");
-
 var input = document.getElementById("secretInput");
 var secretBtn = document.getElementById("secretBtn");
-
 var inventoryText = document.getElementById("inventory");
-
 var currentScene = "opening";
 var textIndex = 0;
-
-var inventory = [];
 var secretUnlocked = false;
 
 var scenes = {
-
- opening:{
-  img:"classroom.jpg",
-  text:[
-"Kat: Man, it's been a hellva day today. I hear we get a new student, though. Maybe she'll be interestin'.",
+opening:{
+ img:"classroom.jpg",
+ text:[
+"Kat: Man, it's been a hellva day today. I hear we get a new student...",
 "Kat: I mean, I sure am popular with tha' ladies. (She is not)",
 "Kat: Maybe this girl will be the one?",
 "*Spechan opens the door, and Kat is overwhelmed by her sheer aura. She's a cute one, isn't she?*",
@@ -34,7 +26,7 @@ var scenes = {
 "Kat: Hi, can I sit next to you guys? I'm Kat, by the way.",
 "Special Week: Yeah, of course! Nice to meet you, Kat.",
 "Beatrice: Yeah, sit down.",
-"*Kat sits down beside Special Week. On her other side is Anna. Kat dramatically yanks her rolly chair off to the side and rolls beside her.*",
+"*Kat sits down beside Special Week.*",
 "Kat: (Whispering) The new girls kinda cute, right! Or am I crazy-I can't be crazy- I mean- I have taste...I th-think...?",
 "Anna: (Whispering) I don't think you're crazy, she very well could be THE finest white girl at WECIB...",
 "Kat: You think I have a chance? Maybe-?",
@@ -49,147 +41,165 @@ var scenes = {
 "Kat: Ah crap...options...ladies...ladies one at a time please...."
   ],
   choices:[
-   {text:"Go with Anna", next:"tcwok"},
-   {text:"Go with Beatrice", next:"sheets"},
-   {text:"Ask Special Week", next:"friends"}
+   {text:"Go with Anna", next:"driveAnna"},
+   {text:"Go with Beatrice", next:"driveBea"},
+   {text:"Stay with Special Week", next:"friends"}
   ]
- },
-
- tcwok:{
-  img:"tcwok.jpg",
-  text:[
-"Kat: Anna, T.C. Wok sounds great, let's go.",
-"Anna: Yay, okay! Let's go, I can drive.",
-"Kat: Wait... this is technically a power fantasy- I should be able to drive.",
-"Kat: No, no, no, I shall drive, my dear.",
-"*They walk to Kat's white 2015 Prius and drive to T.C. Wok.*",
-"Kat: What are you thinking about getting? I kind of want lo mein and a Thai tea... mmmm Thai tea...",
-"Anna: I think I'm gonna get some sort of noodles or soup. Anything with broth.",
-"Kat: Hmmm... Thai tea... sounds good...",
-"*They get their food and sit down.*",
-"Kat: Maybe love just isn't for me.. Doomed to be a loveless chud forever...",
-"Anna: I don't think you are, actually. You know, this is nice, we should hang out like this more often.",
+},
+tcwok:{
+ img:"tcwok.jpg",
+ text:[
+"Kat: I want lo mein and Thai tea",
+"Anna: noodles or soup",
+"Kat: Maybe love just isn't for me..",
+"Anna: I don't think you are",
 "Kat: Is this yaori?",
 "Anna: Yes, absolutely.",
 "Ending one:",
 "Yaori",
 "Platonic love SWEEP!!!"
-  ],
-  end:true
- },
-
- sheets:{
-  img:"sheets.jpg",
-  text:[
-"Kat: Oh, can I come with you, Beatrice? I want a chocolate milk or something.",
+ ],
+ end:true
+},
+sheets:{
+ img:"sheets.jpg",
+ text:[
+"Kat: Oh, can I come with you, Beatrice?",
 "Beatrice: Yeah, cmon.",
-"Kat: Wait... this is technically a power fantasy- I should be able to drive.",
-"Kat: I shall drive, my dear.",
+"Kat: I shall drive.",
 "*They go to Sheets.*",
-"Beatrice: Man, this Depop seller hasn't shipped my order yet, I really want this shirt...",
-"Kat: Have you considered killing them or something? It's been like 3 weeks.",
-"Beatrice: I don't think that'd get me my shirt, honestly.",
-"Kat: ...You're probably right. We're here, anyway.",
-"*Kat struggles parking her Prius.*",
-"Kat: I feel like I should be more embarrassed about my parking skills, but, man, I give up.",
-"Beatrice: Yeah, you probably should.",
-"Kat: I'll pay, m'lady.",
-"*Beatrice cringes.*",
-"Ending 2:",
-"Beatrice has lunch with you, despite your erm- flaws."
-  ],
-  end:true
- },
-
- friends:{
-  img:"lunch.jpg",
-  text:[
+"Beatrice: Depop seller hasn't shipped",
+"Kat: Have you considered killing them",
+"Beatrice: no",
+"Kat: I'll pay",
+"Ending 2: Cringe Ending"
+ ],
+ end:true
+},
+friends:{
+ img:"lunch.jpg",
+ text:[
 "You all have a nice lunch together."
-  ],
-  end:true
- },
-
- secret:{
-  img:"kat.jpg",
-  text:[
-"SECRET ENDING UNLOCKED",
+ ],
+ end:true
+},
+secret:{
+ img:"kat.jpg",
+ text:[
+"SECRET ENDING",
 "Kat: You found me...",
-"Kat: Maybe giving up isn't so bad after all.",
 "The End."
-  ],
-  end:true
- }
-
+ ],
+ end:true
+},
+driveAnna:{
+ img:"tcwok.jpg",
+ text:[
+"Kat: I shall drive.",
+"*They drive to T.C. Wok*"
+ ],
+ choices:[
+  {text:"Drive confidently", next:"tcwok"},
+  {text:"Drive badly", next:"awkDrive"}
+ ]
+},
+awkDrive:{
+ img:"tcwok.jpg",
+ text:[
+"Kat: I almost crashed...",
+"Anna: bruh..."
+ ],
+ choices:[
+  {text:"Continue anyway", next:"tcwok"}
+ ]
+},
+driveBea:{
+ img:"sheets.jpg",
+ text:[
+"Kat: I shall drive."
+ ],
+ choices:[
+  {text:"Park well", next:"sheets"},
+  {text:"Park badly", next:"beaChoice"}
+ ]
+},
+beaChoice:{
+ img:"sheets.jpg",
+ text:[
+"Kat: bad parking...",
+"Beatrice: yeah..."
+ ],
+ choices:[
+  {text:"Laugh it off", next:"sheets"},
+  {text:"Make it worse", next:"ending2"}
+ ]
+},
+secretTrigger:{
+ img:"kat.jpg",
+ text:[
+"Something feels off...",
+"A strange option appears..."
+ ],
+ choices:[
+  {text:"???", next:"secret"}
+ ]
+}
 };
 
-function showText() {
-  var scene = scenes[currentScene];
-
-  p1.textContent = scene.text[textIndex] || "";
-  p2.textContent = scene.text[textIndex + 1] || "";
-  p3.textContent = scene.text[textIndex + 2] || "";
+function showText(){
+ var scene = scenes[currentScene];
+ p1.textContent = scene.text[textIndex] || "";
+ p2.textContent = scene.text[textIndex+1] || "";
+ p3.textContent = scene.text[textIndex+2] || "";
 }
 
-function nextText() {
-  textIndex += 3;
-
-  var scene = scenes[currentScene];
-
-  if (textIndex >= scene.text.length) {
-    nextBtn.style.display = "none";
-    showChoices();
-  }
-
-  showText();
+function nextText(){
+ textIndex += 3;
+ var scene = scenes[currentScene];
+ if(textIndex >= scene.text.length){
+  nextBtn.style.display = "none";
+  showChoices();
+ }
+ showText();
 }
 
-function showChoices() {
-  choicesDiv.innerHTML = "";
-
-  var scene = scenes[currentScene];
-
-  if (!scene.choices) return;
-
-  for (var i = 0; i < scene.choices.length; i++) {
-    var btn = document.createElement("button");
-
-    btn.textContent = scene.choices[i].text;
-
-    btn.onclick = function() {
-      var choiceText = this.textContent;
-
-      for (var j = 0; j < scene.choices.length; j++) {
-        if (scene.choices[j].text === choiceText) {
-          currentScene = scene.choices[j].next;
-          textIndex = 0;
-          img.src = scenes[currentScene].img;
-          nextBtn.style.display = "inline";
-          choicesDiv.innerHTML = "";
-          showText();
-        }
-      }
+function showChoices(){
+ choicesDiv.innerHTML = "";
+ var scene = scenes[currentScene];
+ if(!scene.choices) return;
+ for(var i=0;i<scene.choices.length;i++){
+  var btn = document.createElement("button");
+  btn.textContent = scene.choices[i].text;
+  btn.onclick = (function(choice){
+    return function(){
+      currentScene = choice.next;
+      textIndex = 0;
+      img.src = scenes[currentScene].img;
+      nextBtn.style.display = "inline";
+      showText();
     };
-
-    choicesDiv.appendChild(btn);
-  }
+  })(scene.choices[i]);
+  choicesDiv.appendChild(btn);
+ }
 }
 
 nextBtn.onclick = nextText;
 
-secretBtn.onclick = function() {
-  if (input.value === "prius") {
-    alert("secret unlocked");
-  }
-};
-
-img.onclick = function() {
-  if (img.src.includes("kat.jpg")) {
-    currentScene = "secret";
-    textIndex = 0;
-    img.src = scenes.secret.img;
-    nextBtn.style.display = "inline";
-    showText();
-  }
+secretBtn.onclick = function(){
+ if(input.value === "prius"){
+  secretUnlocked = true;
+  choicesDiv.innerHTML = "";
+  var btn = document.createElement("button");
+  btn.textContent = "???";
+  btn.onclick = function(){
+   currentScene = "secret";
+   textIndex = 0;
+   img.src = scenes.secret.img;
+   nextBtn.style.display = "inline";
+   showText();
+  };
+  choicesDiv.appendChild(btn);
+ }
 };
 
 img.src = scenes[currentScene].img;
